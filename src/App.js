3bc7d4773794809222fc66396d368bfeca4ffcfc
Grace X Wang <grace.wang95@gmail.com>
2022-07-19 22:34:57 -0700

    add font/styling for header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import SearchBox from './SearchBox';
 import CardList from './CardList';
 import { cats } from './cats';
+import './App.css';
 
 
 class App extends Component {
